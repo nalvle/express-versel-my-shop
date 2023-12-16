db44fc30b0c356e4d453287a1df6e5f56ddbe3ff
nalvle <nalvle@live.ru>
2023-12-16 12:36:37 +0200

    fix 001

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
+app.get('/', (req, res)=>{
+    res.send('Test')
+})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
