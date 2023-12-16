d43a580c892a53f402db78fda2ce5b5511564230
nalvle <nalvle@live.ru>
2023-12-16 12:52:20 +0200

    fix 002

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const color = require("colors");
 const app = express();
 
 app.use(express.json({ extended: false }));
@@ -8,4 +9,7 @@ app.get('/', (req, res)=>{
 })
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+app.listen(PORT, () => console.log(
+    color.bgYellow.black('Server is running in port'),
+    color.bgBlue.black(`${PORT}`)
+    ));
