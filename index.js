075a37243efbff863a1989574c31b51b80859494
nalvle <nalvle@live.ru>
2023-12-16 12:27:53 +0200

    fix Deploy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const color = require("colors");
 const app = express();
 const product = require("./api/product");
 
@@ -7,5 +6,5 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(color.america('Server is running in port '+PORT)));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
