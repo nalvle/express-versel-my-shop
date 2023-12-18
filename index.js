72061064799c2dd141963723a1da703c44438cf2
nalvle <nalvle@live.ru>
2023-12-18 21:01:55 +0200

    go to nunjucks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import { config } from 'dotenv'
 import * as path from 'path'
 import methodOverride from 'method-override'
+import nunjucks from "nunjucks"
 import logger, {log, colors, symbols, tag, time, info, error, warn, done} from 'diy-log'
 import userRouter from './routers/users.js'
 
@@ -20,10 +21,14 @@ app.use(methodOverride('X-HTTP-Method')) //          Microsoft
 app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
 app.use(methodOverride('X-Method-Override')) //      IBM
 app.use(express.static(__dirname + '/static/'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+app.set('view engine', 'html')
 
-app.get('/', (req, res)=>{
-    res.sendFile(admin_views+'/index.html');
+nunjucks.configure('views', {autoescape: true,express: app});
+  
+app.get('/', (_, res)=>{
+    res.render('admin/index', {title:'Привет мир !'});
 })
 
 app.use('/users',userRouter)
