8e9b3a26cb3270e92765a4ab8f4d4d264e6fcf83
nalvle <nalvle@live.ru>
2023-12-19 19:20:26 +0200

    add nav

--- index.js
+++ index.js
@@ -2,43 +2,29 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 import * as path from 'path'
-import methodOverride from 'method-override'
 import nunjucks from "nunjucks"
-import logger, {log, colors, symbols, tag, time, info, error, warn, done} from 'diy-log'
+import logger, {done} from 'diy-log'
 
 import main from './routers/main.js'
 import mainAdmin from './routers/mainAdmin.js'
 import userAdmin from './routers/usersAdmin.js'
 
-const app = express();
+const app = express();config()
 const __dirname = path.resolve();
 
-config()
-
 app.use(express.json());
-// override with POST having ?_method=DELETE
-// override with different headers; last one takes precedence
-app.use(methodOverride('X-HTTP-Method')) //          Microsoft
-app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
-app.use(methodOverride('X-Method-Override')) //      IBM
 app.use(express.static(__dirname + '/static/'));
-//app.set('view engine', 'ejs');
-//app.set('view engine', 'pug');
+app.use('/admin/css',express.static(__dirname +'/views/admin/css'));
 app.set('view engine', 'html')
 
 nunjucks.configure('views', {autoescape: true,express: app});
-  
+// Users routers 
 app.use('/',main)
+// Admin routers
 app.use('/admin',mainAdmin)
-
 app.use('/admin/users',userAdmin)
 
-
-
-
 run().then(()=>{done(' db ready');}).catch(error => console.log(error.stack));
-
-
 async function run() {
   await mongoose.connect(process.env.MONGO_URL);
 }
--- routers/usersAdmin.js
+++ routers/usersAdmin.js
@@ -5,32 +5,33 @@ const urlencodedParser = express.urlencoded({extended: false});
 
 const router = new express.Router()
 //create
-router.post("/", urlencodedParser, async (request, response)=> {
-    if(!request.body) return response.sendStatus(400);
+router.post("/", urlencodedParser, async (req, res, next)=> {
+    if(!req.body) return res.sendStatus(400);
         await Customer.create({
-            name:request.body.name,
-            age:request.body.age,
-            email:request.body.email
+            name:req.body.name,
+            age:req.body.age,
+            email:req.body.email
         })
-  response.render('admin/index', {name:request.body.name})
-    //response.send(`${request.body.name} - ${request.body.age}`);
+  //return res.render('admin/users', {name:req.body.name})
+  return res.redirect('/admin/users')
+    //res.send(`${req.body.name} - ${req.body.age}`);
 });
 
 
-router.get('/', async (request, response)=>{
-    response.render('admin/users',{users:await Customer.find()})
-    //response.send(await Customer.find())
+router.get('/', async (req, res)=>{
+    res.render('admin/users',{users:await Customer.find()})
+    //res.send(await Customer.find())
 })
 //get one
 //router.get('/users/:id', PostController.getOne)
 //update
 //router.put('/users', PostController.update)
 //delete
-router.delete('/:id', urlencodedParser, async (request, response)=>{
-    const user = await Customer.findByIdAndDelete(request.params.id)
-    //console.log(request.params.id);
-    //response.json(request.params.id)
-    return response.json({user:user, id:request.params.id})
+router.delete('/:id', urlencodedParser, async (req, res)=>{
+    const user = await Customer.findByIdAndDelete(req.params.id)
+    //console.log(req.params.id);
+    //res.json(req.params.id)
+    return res.json({user:user, id:req.params.id})
 })
 
 export default router;
